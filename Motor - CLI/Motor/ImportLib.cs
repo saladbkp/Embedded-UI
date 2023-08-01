d514500da6ed9fd9f8c40527b712326dad61d77c
saladbkp <saladbkp@gmail.com>
2023-08-01 16:24:25 +0800

    Upload

---
+++ Motor - CLI/Motor/ImportLib.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Motor
+{
+    public class ImportLib
+    {
+        // For Windows Mobile, replace user32.dll with coredll.dll
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+        [DllImport("user32.dll", EntryPoint = "GetParent", SetLastError = true)]
+        public static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+        
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool EnableWindow(IntPtr hWnd, bool bEnable);
+
+        //Gets window attributes
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern int GetWindowLong(IntPtr hWnd, int nIndex);
+        [DllImport("USER32.DLL")]
+        public static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
+        [DllImport("USER32.DLL")]
+        public static extern IntPtr SetParent(IntPtr hWndChild, IntPtr hWndNewParent);
+
+        //assorted constants needed
+        public static int GWL_STYLE = -16;
+        public static int WS_CHILD = 0x40000000; //child window
+        public static int WS_BORDER = 0x00800000; //window with border
+        public static int WS_DLGFRAME = 0x00400000; //window with double border but no title
+        public static int WS_CAPTION = WS_BORDER | WS_DLGFRAME; //window with a title bar
+    }
+}
---
+++ Motor - CLI/Motor/MainWindow.xaml.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml.Linq;
+
+namespace Motor
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Init();
+            
+        }
+
+        #region CMD
+        private void Init()
+        {
+
+            EmbeddedApp embeddedApp = new EmbeddedApp(WinEmb, 100, 100);
+            WinEmb.Child = embeddedApp;
+            
+        }
+        #endregion
+
+    }
+
+    class EmbeddedApp : HwndHost, IKeyboardInputSink
+    {
+        private Border WinHoster;
+        private double screenW, screenH;
+        public EmbeddedApp(Border b, double sW, double sH)
+        {
+            WinHoster = b;
+            screenW = sW;
+            screenH = sH;
+        }
+        protected override HandleRef BuildWindowCore(HandleRef hwndParent)
+        {
+            ProcessStartInfo startInfo = new ProcessStartInfo("C:\\Windows\\System32\\cmd.exe");
+            startInfo.WindowStyle = ProcessWindowStyle.Minimized;
+            Process.Start(startInfo);
+            System.Threading.Thread.Sleep(1500);
+
+            Process[] processes = Process.GetProcessesByName("WindowsTerminal");
+            IntPtr hwnd = processes[0].MainWindowHandle;
+
+            ImportLib.ShowWindow(hwnd, 3);
+            ImportLib.EnableWindow(hwnd, true);
+            int style = ImportLib.GetWindowLong(hwnd, ImportLib.GWL_STYLE);
+            style = style & ~((int)ImportLib.WS_CAPTION) & ~((int)ImportLib.WS_BORDER);
+            style |= ((int)ImportLib.WS_CHILD);
+            ImportLib.SetWindowLong(hwnd, ImportLib.GWL_STYLE, style);
+            ImportLib.SetParent(hwnd, hwndParent.Handle);
+
+            return new HandleRef(this, hwnd);
+        }
+
+        protected override void DestroyWindowCore(HandleRef hwnd)
+        {
+            Console.WriteLine("CLOSE...\n");
+        }
+
+
+    }
+
+
+}
